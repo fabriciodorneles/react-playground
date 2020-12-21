75218917970e316d546c879c76f3fd82b4e12b41
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-12-21 08:16:57 -0300

    functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, {useEffect, useRef} from 'react';
-import Input from './Input';
 
 function App() {
-  const inputRef = useRef(null)
 
-  useEffect(() => {
-    inputRef.current.focus();
-    console.log({inputRef});
-  }, []);
+  function factory() {
+    return (a, b) => a + b;
+  }
+  
+  const sum1 = factory();
+  const sum2 = factory();
+  
+  sum1(1, 2); // => 3
+  sum2(1, 2); // => 3
+  console.log(sum1 === sum1);
+  console.log(sum1 === sum2);
+
   return (
     <div>
-      <Input ref={inputRef} />
+      <div > TESTE </div>
     </div>
   );
 }
--- src/Input.js
+++
@@ -1,5 +0,0 @@
-import React, { forwardRef } from 'react';
-
-const Input = (props, ref) => <input ref={ref} type="text" />;
-
-export default forwardRef(Input);
